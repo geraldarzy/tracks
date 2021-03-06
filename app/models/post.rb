676a7dd51ddabec4a31adca936e270e1f662f0a5
geraldarzy <geraldarzola@gmail.com>
2021-03-06 16:10:35 -0500

    add relationships to comment/user/post models

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,7 @@
 class Post < ActiveRecord::Base
+    belongs_to :station
+    belongs_to :user
+    has_many :comments
+
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,9 @@
 class User < ActiveRecord::Base
+    #show posts a user made
+    has_many :posts
+    #show comments a user made/show who owner of comment is
+    has_many :comments
+    #show stations a user has commented on
+    has_many :stations, through: :posts
 
 end
