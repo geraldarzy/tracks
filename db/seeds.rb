40ba95e4c849adc621517cb3ff5e2b930537c67d
geraldarzy <geraldarzola@gmail.com>
2021-03-06 16:25:34 -0500

    add data to seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+r = Track.create(name:"R Track")
+grand = Station.create(name:"Grand Ave. Station", address: "Queens Blvd. Elmhurst, Queens 11373")
+woodhaven = Station.create(name:"Woodhaven Station", address: "Woodhaven Blvd. Elmhurst, Queens 11373")
+TrackStation.create(station_id: 1, track_id: 1)
+TrackStation.create(station_id: 2, track_id: 1)
+arzy = User.create(username:"Arzy", first_name:"Gerald", last_name:"Arzola", email:"arzy@arzola.com")
+arzyspost = Post.create(title:"Arzy's Post about Grand Ave", content: "Hello my name is arzy and this is about Grand ave.", station_id: 1, user_id:1)
