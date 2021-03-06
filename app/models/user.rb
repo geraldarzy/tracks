dbf72cca18d8016914c1a1aa0c0e5b785cce98d9
geraldarzy <geraldarzola@gmail.com>
2021-03-06 18:32:29 -0500

    add devise to users

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,9 @@
 class User < ActiveRecord::Base
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+         
     #show posts a user made
     has_many :posts
     #show comments a user made/show who owner of comment is
