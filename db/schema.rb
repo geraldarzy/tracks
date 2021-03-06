7657e3d9efe2dfe24f1391b8b69f5f380bb7e169
geraldarzy <geraldarzola@gmail.com>
2021-03-06 13:43:29 -0500

    make post class and table

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_06_183728) do
+ActiveRecord::Schema.define(version: 2021_03_06_184251) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "posts", force: :cascade do |t|
+    t.string "title"
+    t.string "content"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "first_name"
