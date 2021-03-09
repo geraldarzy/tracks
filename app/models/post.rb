8fd215b1425e15472e9b2d6eaff6cde90ea59a06
geraldarzy <geraldarzola@gmail.com>
2021-03-09 18:27:12 -0500

    add post method to tell when post was made relative of todays date and time

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,5 +3,11 @@ class Post < ActiveRecord::Base
     belongs_to :user
     has_many :comments
 
-
+    def relative_time
+        if self.created_at.between?(Time.zone.now.beginning_of_day, Time.zone.now.end_of_day)
+            "Posted today at #{self.created_at.strftime("%I:%M %p")}"
+        else
+            "Posted on #{self.created_at.strftime("%B %d, %Y at %I:%M %p")}"
+        end
+    end
 end
