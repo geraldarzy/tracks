ce2a0b8bf2211218bd4f6eca4d2a4100fb5a672c
geraldarzy <geraldarzola@gmail.com>
2021-03-08 20:01:59 -0500

    add show action to stations_controller

--- app/controllers/stations_controller.rb
+++ app/controllers/stations_controller.rb
@@ -2,4 +2,8 @@ class StationsController < ApplicationController
     def index
         @stations = Station.all
     end
+
+    def show
+        @station = Station.find_by(id: params[:id])
+    end
 end
