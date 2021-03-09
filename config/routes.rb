e391c0c3a317663b1ecc4a499c4462033ed40352
geraldarzy <geraldarzola@gmail.com>
2021-03-09 18:10:06 -0500

    add nested post routes to stations

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
 
-  devise_for :users, controllers: { registrations: 'registrations' }
-  resources :users
+  devise_for :users, controllers: { registrations: 'registrations' } 
+  resources :users 
 
-resources :stations, only: [:index,:show]
+  resources :stations, only: [:index,:show] do
+    resources :posts, only: [:show]
+  end
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
