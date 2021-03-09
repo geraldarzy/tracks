35d7844f36294113bdb8fdaff78ff5d96f3fd547
geraldarzy <geraldarzola@gmail.com>
2021-03-09 10:19:04 -0500

    fix migrations; pre-existing users columns colliding with devise columns

--- db/migrate/20210306183728_create_users.rb
+++ db/migrate/20210306183728_create_users.rb
@@ -4,8 +4,6 @@ class CreateUsers < ActiveRecord::Migration[6.0]
       t.string :first_name
       t.string :last_name
       t.string :username
-      t.string :email
-      t.string :password_digest
     end
   end
 end
