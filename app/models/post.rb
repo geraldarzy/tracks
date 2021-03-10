a91b885020ac2425c00f34a8c22dfa0888a4456d
geraldarzy <geraldarzola@gmail.com>
2021-03-09 21:48:39 -0500

    add validators to create a post

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,6 +3,10 @@ class Post < ActiveRecord::Base
     belongs_to :user
     has_many :comments
 
+    validates :title, presence:true
+    validates :description, presence:true
+    validates :content, presence:true
+
     def relative_time
         if self.created_at.between?(Time.zone.now.beginning_of_day, Time.zone.now.end_of_day)
             "Posted today at #{self.created_at.strftime("%I:%M %p")}"
