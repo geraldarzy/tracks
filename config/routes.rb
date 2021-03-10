57f86bf6818e609169c8e11a7ed7d4c294eff8a9
geraldarzy <geraldarzola@gmail.com>
2021-03-09 22:15:57 -0500

    add track show route

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   devise_for :users, controllers: { registrations: 'registrations' } 
   resources :users 
 
-  resources :tracks, only: [:index]
+  resources :tracks, only: [:index,:show]
 
   resources :stations, only: [:index,:show] do
     resources :posts, only: [:show,:new,:create]
