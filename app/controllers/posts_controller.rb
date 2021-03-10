2e3b2e19f964e09a661f5e76c9607aa0b7000213
geraldarzy <geraldarzola@gmail.com>
2021-03-09 21:07:05 -0500

    make create action for nested post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -11,8 +11,24 @@ class PostsController < ApplicationController
     end
 
     def new
-        @station = Station.find_by(id: params[:station_id])
+        set_station
         @post = @station.posts.new(user_id:current_user.id)
     end
 
+    def create
+        set_station
+        params[:post][:user_id] = current_user.id
+        @post = @station.posts.create(post_params(:title,:description,:content,:station_id,:user_id))
+        redirect_to "/stations/#{@station.id}"
+    end
+
+    private
+
+    def set_station
+        @station = Station.find_by(id: params[:station_id])
+    end
+
+    def post_params(*args)
+        params.require(:post).permit(*args)
+    end
 end
