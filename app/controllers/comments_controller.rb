41afff3478dbc97da92ca0cbaf1e0a49124ef2f5
geraldarzy <geraldarzola@gmail.com>
2021-03-11 15:07:09 -0500

    make create action for comments_controller

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -3,6 +3,16 @@ class CommentsController < ApplicationController
         @comment = Post.find_by(id:params[:post_id]).comments.new
     end
 
-  
+    def create
+        @comment = Post.find_by(id:params[:post_id]).comments.new(comment_params(:content))
+        @comment.user_id = current_user.id
+        if @comment.save
+            redirect_to station_post_path(@comment.post.station,@comment.post)
+        else
+            render 'new'
+        end
+    end
+
+ 
     
 end
