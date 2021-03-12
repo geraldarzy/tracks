ba794712092c95f6905a1b899d02230ca72bf8c6
geraldarzy <geraldarzola@gmail.com>
2021-03-12 11:42:55 -0500

    determine if user_signed_in? and show homepage according to signed in or not

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,10 @@
 class ApplicationController < ActionController::Base
     def home
+        if user_signed_in?
+            render "loggedin"
+        else
+            render "notloggedin"
+        end
     end
     
     def after_sign_out_path_for(resource_or_scope)
