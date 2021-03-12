c1303db4a2e524755a305a50a0c952245dadab9c
geraldarzy <geraldarzola@gmail.com>
2021-03-12 11:52:43 -0500

    fix adding ' Track' to track name, append ' Track' in seeds file instead of before_save method

--- app/models/track.rb
+++ app/models/track.rb
@@ -2,9 +2,5 @@ class Track<ActiveRecord::Base
     has_many :track_stations
     has_many :stations, through: :track_stations
 
-    before_save :add_track_to_name
-
-    def add_track_to_name
-        self.name = self.name + " Track"
-    end
+ 
 end
