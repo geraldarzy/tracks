2672e544c537291f3e3f00aff21078d48c8e2dcd
geraldarzy <geraldarzola@gmail.com>
2021-03-12 11:25:42 -0500

    add ' Track' to track names before_save

--- app/models/track.rb
+++ app/models/track.rb
@@ -1,4 +1,10 @@
 class Track<ActiveRecord::Base
     has_many :track_stations
     has_many :stations, through: :track_stations
+
+    before_save :add_track_to_name
+
+    def add_track_to_name
+        self.name = self.name + " Track"
+    end
 end
