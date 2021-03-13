27272d5dee2e82b51eb4579ce456bd187126f730
geraldarzy <geraldarzola@gmail.com>
2021-03-13 17:10:57 -0500

    add pg_search by name method

--- app/models/station.rb
+++ app/models/station.rb
@@ -1,8 +1,19 @@
 class Station < ActiveRecord::Base
+    include PgSearch::Model
+
     has_many :track_stations
     has_many :tracks, through: :track_stations
     has_many :posts
     has_many :users, through: :posts
+
+    validates :name, uniqueness: true
+
+    pg_search_scope :search_by_name, against: :name,
+    using: {
+        tsearch: {
+          prefix: true
+        }
+      }
 
     def posts_made_within_24hrs
         self.posts.where(created_at: Time.zone.now.beginning_of_day..Time.zone.now.end_of_day)
