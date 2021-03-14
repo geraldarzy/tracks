bd0686f9a97b8e59acd50cf926f2b0c9b3893de8
geraldarzy <geraldarzola@gmail.com>
2021-03-14 03:24:17 -0400

    add :home_station column to users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_13_224713) do
+ActiveRecord::Schema.define(version: 2021_03_14_055336) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -66,6 +66,7 @@ ActiveRecord::Schema.define(version: 2021_03_13_224713) do
     t.datetime "remember_created_at"
     t.string "provider", limit: 50, default: "", null: false
     t.string "uid", limit: 50, default: "", null: false
+    t.string "home_station"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
